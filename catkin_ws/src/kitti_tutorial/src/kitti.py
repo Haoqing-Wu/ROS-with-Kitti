46fed14be4158df97c123bf0a48f33a38cb30876
Haoqing <haoqing.wu@outlook.com>
2023-03-16 17:56:17 +0100

    Add imu and gps publish

--- catkin_ws/src/kitti_tutorial/src/kitti.py
+++ catkin_ws/src/kitti_tutorial/src/kitti.py
@@ -3,15 +3,16 @@ import os
 from data_utils import *
 from publish_utils import *
 
-DATA_PATH = '/home/tf77wwl/ROS-with-Kitti/data/2011_09_26/2011_09_26_drive_0011_sync/'
+DATA_PATH = '/home/tf77wwl/ROS-with-Kitti/data/2011_09_26/2011_09_26_drive_0005_sync/'
 
 if __name__ == '__main__':
     frame = 0
     rospy.init_node('kitti_node', anonymous=True)
     cam_pub = rospy.Publisher('kitti_cam', Image, queue_size=10)
     pcl_pub = rospy.Publisher('kitti_point_cloud', PointCloud2, queue_size=10)
-    ego_pub = rospy.Publisher('kitti_ego_car', Marker, queue_size=10)
-    model_pub = rospy.Publisher('kitti_car_model', Marker, queue_size=10)
+    ego_pub = rospy.Publisher('kitti_ego_car', MarkerArray, queue_size=10)
+    imu_pub = rospy.Publisher('kitti_imu', Imu, queue_size=10)
+    gps_pub = rospy.Publisher('kitti_gps', NavSatFix, queue_size=10)
     bridge = CvBridge()
 
     rate = rospy.Rate(10)
@@ -24,10 +25,13 @@ if __name__ == '__main__':
         publish_point_cloud(pcl_pub, point_cloud)
 
         publish_ego_car(ego_pub)
-        publish_car_model(model_pub)
-
+        
+        imu_data = read_imu(os.path.join(DATA_PATH, 'oxts/data/%010d.txt'%frame))
+        publish_imu(imu_pub, imu_data)
+        publish_gps(gps_pub, imu_data)
+        
         rospy.loginfo("Camera image and point cloud published")
         frame += 1
-        frame %= 233
+        frame %= 154
         rate.sleep()
 
--- catkin_ws/src/kitti_tutorial/src/publish_utils.py
+++ catkin_ws/src/kitti_tutorial/src/publish_utils.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Header
-from visualization_msgs.msg import Marker
+from visualization_msgs.msg import Marker, MarkerArray
 import sensor_msgs.point_cloud2 as pcl2
 from geometry_msgs.msg import Point
-from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs.msg import Image, PointCloud2, Imu, NavSatFix
 from cv_bridge import CvBridge
-import tf
+import tf_conversions as tf
 import numpy as np
 
 FRAME_ID = 'map'
@@ -24,6 +24,8 @@ def publish_ego_car(ego_car_pub):
     """
     Publish left and right 45 degree FOV lines and ego car model mesh
     """
+
+    marker_array = MarkerArray()
     marker = Marker()
     marker.header.frame_id = FRAME_ID
     marker.header.stamp = rospy.Time.now()
@@ -44,9 +46,7 @@ def publish_ego_car(ego_car_pub):
     marker.points.append(Point(0, 0, 0))
     marker.points.append(Point(10, 10, 0))
 
-    ego_car_pub.publish(marker)
-
-def publish_car_model(model_pub):
+    marker_array.markers.append(marker)
 
     mesh_marker = Marker()
     mesh_marker.header.frame_id = FRAME_ID
@@ -55,7 +55,7 @@ def publish_car_model(model_pub):
     mesh_marker.id = -1
     mesh_marker.lifetime = rospy.Duration()
     mesh_marker.type = Marker.MESH_RESOURCE
-    mesh_marker.mesh_resource = "package://kitti_tutorial/ego/R8.dae"
+    mesh_marker.mesh_resource = "package://kitti_tutorial/ego/Prius.dae"
 
     mesh_marker.pose.position.x = 0.0
     mesh_marker.pose.position.y = 0.0
@@ -67,13 +67,46 @@ def publish_car_model(model_pub):
     mesh_marker.pose.orientation.z = q[2]
     mesh_marker.pose.orientation.w = q[3]
 
-    mesh_marker.color.r = 0.0
-    mesh_marker.color.g = 1.0
-    mesh_marker.color.b = 1.0
+    mesh_marker.color.r = 0.5
+    mesh_marker.color.g = 0.5
+    mesh_marker.color.b = 0.5
     mesh_marker.color.a = 1.0
 
-    mesh_marker.scale.x = 0.9
-    mesh_marker.scale.y = 0.9
-    mesh_marker.scale.z = 0.9
+    mesh_marker.scale.x = 1.0
+    mesh_marker.scale.y = 1.0
+    mesh_marker.scale.z = 1.0
 
-    model_pub.publish(mesh_marker)
+    marker_array.markers.append(mesh_marker)
+
+    ego_car_pub.publish(marker_array)
+
+def publish_imu(imu_pub, imu_data):
+    imu = Imu()
+    imu.header.frame_id = FRAME_ID
+    imu.header.stamp = rospy.Time.now()
+
+    q = tf.transformations.quaternion_from_euler(float(imu_data.roll), float(imu_data.pitch), float(imu_data.yaw))
+    imu.orientation.x = q[0]
+    imu.orientation.y = q[1]
+    imu.orientation.z = q[2]
+    imu.orientation.w = q[3]
+    imu.linear_acceleration.x = imu_data.af
+    imu.linear_acceleration.y = imu_data.al
+    imu.linear_acceleration.z = imu_data.au
+    imu.angular_velocity.x = imu_data.wf
+    imu.angular_velocity.y = imu_data.wl
+    imu.angular_velocity.z = imu_data.wu
+
+    imu_pub.publish(imu)
+    
+def publish_gps(gps_pub, imu_data):
+    gps = NavSatFix()
+    gps.header.frame_id = FRAME_ID
+    gps.header.stamp = rospy.Time.now()
+
+    gps.latitude = imu_data.lat
+    gps.longitude = imu_data.lon
+    gps.altitude = imu_data.alt
+
+    gps_pub.publish(gps)
+    
